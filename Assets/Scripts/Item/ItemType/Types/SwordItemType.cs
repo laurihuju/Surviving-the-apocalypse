29248db11cb9d0c21172e4c58d7cc3e3221870e3
laurihuju <s2000949@edu.bc.fi>
2022-04-12 10:28:47 +0300

    Paranneltu pelaajan hyökkäystä Korjattu myös ongelma, jossa pelaajan kädet tärisivät liikkuessa.

--- Assets/Scripts/Item/ItemType/Types/SwordItemType.cs
+++ Assets/Scripts/Item/ItemType/Types/SwordItemType.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 
 public class SwordItemType : UsableItem
@@ -5,9 +6,18 @@ public class SwordItemType : UsableItem
     [Header("Animation")]
     [SerializeField] private Animator armAnimator;
 
+    [Header("Sword")]
+    [SerializeField] private Transform damagePoint;
+    [SerializeField] private float damageDistance;
+    [SerializeField] private float damageTime;
+    [SerializeField] private float damage;
+    [SerializeField] private float cooldown;
+
+    private float nextAttackTime = 0;
+
     public override bool CanUse()
     {
-        return true;
+        return Time.time > nextAttackTime;
     }
 
     public override void OnCollect()
@@ -32,6 +42,28 @@ public class SwordItemType : UsableItem
 
     public override void Use()
     {
+        StartCoroutine(Attack());
+    }
+
+    private IEnumerator Attack()
+    {
         armAnimator.SetTrigger("Attack");
+
+        nextAttackTime = Time.time + cooldown;
+
+        yield return new WaitForSeconds(damageTime);
+
+        Collider[] targets = Physics.OverlapSphere(damagePoint.position, damageDistance);
+        for(int i = 0; i < targets.Length; i++)
+        {
+            if (!targets[i].CompareTag("Enemy"))
+                continue;
+
+            HealthManager health = targets[i].GetComponent<HealthManager>();
+            if (health == null)
+                continue;
+
+            health.ChangeHealth(-damage);
+        }
     }
 }
