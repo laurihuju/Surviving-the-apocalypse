8ff4f143a14c6e432d3d97fe5568f43e24ef65f4
Huju Lauri <s2000949@edu.bc.fi>
2022-05-04 12:42:50 +0300

    Tehty craftayspisteet

--- Assets/Scripts/Item/Crafting/CraftingStation.cs
+++ Assets/Scripts/Item/Crafting/CraftingStation.cs
@@ -3,4 +3,26 @@ using UnityEngine;
 public class CraftingStation : MonoBehaviour
 {
     [SerializeField] private CraftingRecipe[] recipes;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!other.CompareTag("Player"))
+            return;
+
+        for(int i = 0; i < recipes.Length; i++)
+        {
+            CraftingMenu.GetInstance().AddRecipe(recipes[i]);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (!other.CompareTag("Player"))
+            return;
+
+        for (int i = 0; i < recipes.Length; i++)
+        {
+            CraftingMenu.GetInstance().RemoveRecipe(recipes[i]);
+        }
+    }
 }
